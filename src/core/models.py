3e75298722abd6516d058784f51c169e030f034d
marcassim <marcelo.assim@gmail.com>
2022-05-18 17:50:43 -0300

    Criação da models EVENTO

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100, verbose_name="Título")
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name="Data do Evento")
+    local = models.TextField(blank=True, null=True)
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name="Data de Criação")
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "Evento"
+        managed = True
+        verbose_name = "evento"
+        verbose_name_plural = "eventos"
+
+        def __str__(self):
+            return self.titulo
