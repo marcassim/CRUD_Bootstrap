9243ead0c19651660532aecd880f9b37408dae4c
marcassim <marcelo.assim@gmail.com>
2022-05-19 18:11:10 -0300

    Atualização dos arquivos: MODELS / VIEWS / URLS

--- src/core/models.py
+++ src/core/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+from datetime import datetime, timedelta
+
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Evento(models.Model):
@@ -18,3 +20,18 @@ class Evento(models.Model):
 
         def __str__(self):
             return self.titulo
+
+
+def get_data_evento(self):
+    return self.data_evento.strftime("%d/%m/%Y %H:%M")
+
+
+def get_evento_atrasado(self):
+    return self.data_evento < datetime.now()
+
+
+def get_evento_que_falta_menos_de_1h(self):
+    return (
+        datetime.now() > self.data_evento - timedelta(hours=1)
+        and datetime.now() < self.data_evento
+    )
